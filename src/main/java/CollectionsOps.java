3ebceed92f71a6334c37f2e89786b80f10b34cb3
Trotyl Yu <yzj1995@vip.qq.com>
2015-11-19 00:36:34 +0800

    Implemented.

--- src/main/java/CollectionsOps.java
+++ src/main/java/CollectionsOps.java
@@ -1,16 +1,17 @@
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CollectionsOps {
 
     public static List<Integer> odd(List<Integer> numbers) {
-        return null;
+        return numbers.stream().filter(i -> i % 2 != 0).collect(Collectors.toList());
     }
 
     public static List<Integer> doubleEvens(List<Integer> numbers) {
-        return null;
+        return numbers.stream().map(i -> i % 2 == 0? 2 * i: i).collect(Collectors.toList());
     }
 
     public static String tryFind(Integer toFind, List<Integer> numbers) {
-        return null;
+        return numbers.stream().anyMatch(i -> i.equals(toFind))? "Hello, " + toFind: "Not Found";
     }
 }
