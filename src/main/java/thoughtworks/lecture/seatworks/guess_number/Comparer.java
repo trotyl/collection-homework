9124a691f97c6ab902a5dea9222be384b5064c00
Trotyl Yu <yzj1995@vip.qq.com>
2015-11-22 21:37:46 +0800

    Refactor implementation of comparer.

--- src/main/java/thoughtworks/lecture/seatworks/guess_number/Comparer.java
+++ src/main/java/thoughtworks/lecture/seatworks/guess_number/Comparer.java
@@ -10,17 +10,17 @@ public class Comparer {
 
     public String compare(String systemNumber, String userNumber) {
         int numberOfAs = 0, numberOfAsAndBs;
-        HashSet<Character> charMap = new HashSet<>();
+        HashSet<Character> set = new HashSet<>();
 
         for (int i = 0; i < systemNumber.length(); i++) {
             if (systemNumber.charAt(i) == userNumber.charAt(i)) {
                 numberOfAs++;
             }
-            charMap.add(systemNumber.charAt(i));
-            charMap.add(userNumber.charAt(i));
+            set.add(systemNumber.charAt(i));
+            set.add(userNumber.charAt(i));
         }
 
-        numberOfAsAndBs = 8 - charMap.size();
+        numberOfAsAndBs = 8 - set.size();
 
         return numberOfAs + "A" + (numberOfAsAndBs - numberOfAs) + "B";
     }
