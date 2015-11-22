6af8415dc8727a7c87e520d74707c20ee5f66c1e
Trotyl Yu <yzj1995@vip.qq.com>
2015-11-22 19:25:36 +0800

    Refactor the collection homework.

--- src/main/java/thoughtworks/lecture/homeworks/collection/CollectionsOps.java
+++ src/main/java/thoughtworks/lecture/homeworks/collection/CollectionsOps.java
@@ -1,21 +1,21 @@
 package thoughtworks.lecture.homeworks.collection;
 
 import java.util.List;
-import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 public class CollectionsOps {
 
     public static List<Integer> odd(List<Integer> numbers) {
-        return numbers.stream().filter(i -> i % 2 != 0).collect(Collectors.toList());
+        return numbers.stream().filter(i -> i % 2 != 0).collect(toList());
     }
 
     public static List<Integer> doubleEvens(List<Integer> numbers) {
-        return numbers.stream().map(i -> i % 2 == 0 ? 2 * i : i).collect(Collectors.toList());
+        return numbers.stream().map(i -> i % 2 == 0 ? 2 * i : i).collect(toList());
     }
 
     public static String tryFind(Integer toFind, List<Integer> numbers) {
-
-        return numbers.stream().filter(i -> i.equals(toFind)).map(integer -> "Hello, " + integer).findFirst().orElse("Not Found");
-//        return numbers.stream().anyMatch(i -> i.equals(toFind)) ? "Hello, " + toFind : "Not Found";
+//        return numbers.stream().filter(i -> i.equals(toFind)).map(integer -> "Hello, " + integer).findFirst().orElse("Not Found");
+        return numbers.stream().anyMatch(i -> i.equals(toFind)) ? "Hello, " + toFind : "Not Found";
     }
 }
